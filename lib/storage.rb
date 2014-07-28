1434213e1a08af88f3483692538276e91d7ceb59
David Good <david_good@pharmcomp.com>
2014-07-28 11:18:17 -0700

    extended storate

--- lib/storage.rb
+++ lib/storage.rb
@@ -1,15 +1,41 @@
 module Storage
 
 class Storage
+  def self.register(name, clazz)
+    @storages ||= {}
+    @storages[name.to_s] = clazz
+    @storages[name.to_s.to_sym] = clazz
+  end
+
+  def store(options = {})
+  end
 end
 
-class MongoStorage < Storage
+class EncryptedStorage
+  def store(options = {})
+  end
 end
 
-class FileStorage < Storage
+class MongoStorage
+  def store(options = {})
+  end
 end
 
-class S3Storage < Storage
+class FileStorage
+  def store(options = {})
+  end
+end
+
+class S3Storage
+  def store(options = {})
+  end
+end
+
+class ClassMethods
+end
+
+def self.included(base)
+  base.extend(ClassMethods)
 end
 
 end
